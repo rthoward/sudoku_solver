3ca643613a54e9cf45a834dae714053775c7510a
a-out <richbone227@gmail.com>
2014-04-10 16:29:35 -0400

    implemented self.Box, added tests
       - determines which box, or region the cell inhabits

--- lib/cell.rb
+++ lib/cell.rb
@@ -6,6 +6,8 @@ module Sudoku
 		# [1] - value of the cell. 0 == blank cell
 		def initialize(value, x, y)
 			@col, @row = x, y
+			@box = self.box(x, y)
+
 			if value == 0
 				@possibilities = Set.new(Array(1..9))
 				@value = nil
@@ -26,6 +28,13 @@ module Sudoku
 		def remove_possibilities!(eliminated)
 			@possibilities = @possibilities.subtract(eliminated)
 		end
+
+		def self.box(x, y)
+			box_col = ((x - 1) / 3).floor + 1     # (1 / 3).floor + 1 | 1
+			box_row = ((y - 1) / 3).floor + 1     # 1
+			( (box_row - 1) * 3) + 1 * box_col  # 3 * 1
+ 		end
+
 
 	end
 
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -21,5 +21,17 @@ describe 'Cell' do
 				cell.possibilities.should eq Set.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
 			end
 		end
+
+	end
+
+	describe 'self.box' do
+		it "takes an x, y position and returns the box the cell inhabits" do
+			Sudoku::Cell.box(1, 1).should eq 1
+			Sudoku::Cell.box(3, 1).should eq 1
+			Sudoku::Cell.box(4, 1).should eq 2
+			Sudoku::Cell.box(1, 4).should eq 4
+			Sudoku::Cell.box(9, 9).should eq 9
+			Sudoku::Cell.box(5, 5).should eq 5
+		end
 	end
 end
