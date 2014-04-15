f3b670dfa5251e80cd63c86fa4c9847ca2d6a31c
a-out <richbone227@gmail.com>
2014-04-14 23:53:26 -0400

    refactored Sudoku::Cell to be more functional
    	- remove_possibilities returns a new cell, rather than
    	  changing state
    	- #initialize now has a parameter for possibilities

--- lib/cell.rb
+++ lib/cell.rb
@@ -6,10 +6,15 @@ module Sudoku
 		attr_accessor :row, :col, :box
 
 		# [1] - value of the cell. 0 == blank cell
-		def initialize(value, x, y)
+		def initialize(value, x, y, possibilities = nil)
 			@col, @row = x, y
 
 			@box = Cell.box(x, y)
+
+			if !possibilities.nil?
+				@possibilities = possibilities
+				return
+			end
 
 			if value == 0
 				@possibilities = Set.new(Array(1..9))
@@ -32,33 +37,19 @@ module Sudoku
 			num_possibilities == 1
 		end
 
-		def try_solve
-			if solvable?
-				@value = @possibilities.to_a[0]
-				true
-			else
-				false
-			end
-		end
-
-		def remove_possibilities!(eliminated)
-			@possibilities = @possibilities.subtract(eliminated)
-		end
-
-		def possibilities
-			@possibilities
-		end
-
 		def num_possibilities
 			@possibilities.size
 		end
 
-		def self.box(x, y)
-			box_col = ((x - 1) / 3).floor + 1     # (1 / 3).floor + 1 | 1
-			box_row = ((y - 1) / 3).floor + 1     # 1
-			( (box_row - 1) * 3) + 1 * box_col  # 3 * 1
- 		end
+		def remove_possibilities(eliminated)
+			Cell.new(@value, @col, @row, @possibilities.subtract(eliminated))
+		end
 
+		def self.box(x, y)
+			box_col = ((x - 1) / 3).floor + 1
+			box_row = ((y - 1) / 3).floor + 1
+			( (box_row - 1) * 3) + 1 * box_col
+ 		end
 
 	end
 
